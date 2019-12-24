91efa1f2eaf305263dcb41c0b9e802c363de5338
Adrian Li <li.adrianmc@gmail.com>
2019-12-24 03:53:41 -0500

    add feed and Link to graphql schema

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
