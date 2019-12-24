b7f7c572832e5224176cc106f8447a9e2e288b37
Adrian Li <li.adrianmc@gmail.com>
2019-12-24 02:54:14 -0500

    add graphql-yoga server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-const hello = (person: string = "World") => console.log(`Hello ${person}!`)
+const { GraphQLServer } = require('graphql-yoga')
 
-hello()         // Hello World!
-hello("Alice")  // Hello Alice!
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
