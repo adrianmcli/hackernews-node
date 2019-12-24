47253a45b70ae783806ab363f521579d3898d261
Adrian Li <li.adrianmc@gmail.com>
2019-12-24 03:05:41 -0500

    format with Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
 
 const typeDefs = `
 type Query {
   info: String!
 }
-`
+`;
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+    info: () => `This is the API of a Hackernews Clone`,
+  },
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-})
+});
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`));
